e7f5ff56e2c1a87a769762a6eda0a6fe22bd06b7
iwan <1322677864@qq.com>
2023-12-22 18:10:32 +0800

    ix

--- study/src/app.controller.ts
+++ study/src/app.controller.ts
@@ -1,4 +1,20 @@
-import { Controller, Get, Inject, Logger, Query, Session, UseGuards, UseInterceptors } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpStatus,
+  Inject,
+  Logger,
+  Query,
+  Session,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import {
+  ApiOperation,
+  ApiParam,
+  ApiQuery,
+  ApiResponse,
+} from '@nestjs/swagger/dist';
 import { Aaa } from './aaa.decorator';
 import { AaaGuard } from './aaa.guard';
 import { AaaService } from './aaa/aaa.service';
@@ -11,7 +27,6 @@ import { UserService } from './user/user.service';
 import { ValidatePipe } from './validate.pipe';
 import { WINSTON_LOGGER_TOKEN } from './winston/winston.module';
 
-
 // 控制器
 @Controller()
 export class AppController {
@@ -19,71 +34,109 @@ export class AppController {
     // 构造注入服务
     private readonly appService: AppService,
     // @Inject("user") private readonly userService:UserService,
-    @Inject(AaaService) private readonly aaa:AaaService
-    ) {}
-    @Inject(WINSTON_LOGGER_TOKEN)
-    private logger;
-    // 属性注入服务
-    @Inject(PersonService)
-    personService:PersonService
-
-   
+    @Inject(AaaService) private readonly aaa: AaaService,
+  ) {}
+  @Inject(WINSTON_LOGGER_TOKEN)
+  private logger;
+  // 属性注入服务
+  @Inject(PersonService)
+  personService: PersonService;
 
   // api
+  @ApiOperation({ summary: '测试 /', description: 'hello world' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'hello world 成功',
+    type: String,
+  })
   @Get()
   getHello(): string {
     // this.userService.create({
     //   username:"iwan"
     // })
-  
+
     return this.appService.getHello();
   }
-  @Get("sss")
-  async getSession(@Session() session){
+  @Get('sss')
+  async getSession(@Session() session) {
     session.count = session.count ? session.count + 1 : 1;
-    console.log(session)
-    return session.count
+    console.log(session);
+    return session.count;
   }
 
-  @Get("aa")
+  @Get('aa')
   @UseGuards(LoginGuard)
-  async getAad(){
-    return "aaaa"
+  @ApiOperation({ summary: '测试 aa 接口', description: 'aaa' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'hello world 成功',
+    type: String,
+  })
+  @ApiQuery({
+    name: 'a1',
+    type: String,
+    description: 'a1 param',
+    required: false,
+    example: '1111',
+  })
+  @ApiQuery({
+    name: 'a2',
+    type: Number,
+    description: 'a2 param',
+    required: true,
+    example: '222',
+  })
+  async getAad() {
+    return 'aaaa';
   }
 
-  @Get("bb")
+  @ApiOperation({ summary: '测试 bb', description: 'bb 描述' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'bbb 成功',
+    type: String,
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'id 不合法'
+})
+  @ApiParam({
+    name: 'id',
+    description: 'ID',
+    required: true,
+    example: 222,
+  })
+  @Get('bb/:id')
   @UseGuards(LoginGuard)
-  async getbbb(){
-    return "bbbb"
+  async getbbb() {
+    return 'bbbb';
   }
-  @Get("profile.json")
+  @Get('profile.json')
   @UseInterceptors(QuerypageInterceptor)
   getProfile(): string {
     // this.userService.create({
     //   username:"iwan"
     // })
-    return "profile"
-  }
-  
-  @UseGuards(LoginGuard)
-  @Get("login")
-  login():string{
-    return "login success"
+    return 'profile';
   }
 
-  @Get("num")
-  async getNum(@Query('num',ValidatePipe) num){
-    console.log(num)
+  @UseGuards(LoginGuard)
+  @Get('login')
+  login(): string {
+    return 'login success';
+  }
+
+  @Get('num')
+  async getNum(@Query('num', ValidatePipe) num) {
+    console.log(num);
     return num;
   }
 
-
-  
-  @Get("aaaa")
-  @Aaa("admin")
+  @Get('aaaa')
+  @Aaa('admin')
   @UseGuards(AaaGuard)
-  async getaaa(@Query('num') num){
-    this.logger.log(`get aaaa`)
-    return "aaaa";
+  async getaaa(@Query('num') num) {
+    this.logger.log(`get aaaa`);
+    return 'aaaa';
   }
 }
--- study/src/main.ts
+++ study/src/main.ts
@@ -5,46 +5,58 @@ import { join } from 'path';
 import { NextFunction, Request, Response } from 'express';
 import { MyLogger } from './MyLogger';
 import { WINSTON_LOGGER_TOKEN } from './winston/winston.module';
-import * as session from "express-session"
+import * as session from 'express-session';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  // 设置静态文件 页面 
-  app.useStaticAssets(join(__dirname,"..","public"),{
-    prefix:"/static"
-  }) 
+  // 设置静态文件 页面
+  app.useStaticAssets(join(__dirname, '..', 'public'), {
+    prefix: '/static',
+  });
 
-  app.use(function (req:Request,res:Response,next:NextFunction){
-    console.log(req.url,"before")
-    next()
-    console.log("after")
-  })
+  app.use(function (req: Request, res: Response, next: NextFunction) {
+    console.log(req.url, 'before');
+    next();
+    console.log('after');
+  });
 
   // session 登录
 
   app.enableCors();
 
-  app.use(session({
-    secret:"iwan", 
-    resave: false,
-    saveUninitialized: false,
-     
-  }))
+  // 配置接口文档
 
+  const config = new DocumentBuilder()
+    .setTitle('study接口文档')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('admin')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+  app.use(
+    session({
+      secret: 'iwan',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
 
   // app.useGlobalPipes(new ValidationPipe())
 
-//   resave 为 true 是每次访问都会更新 session，不管有没有修改 session 的内容，而 false 是只有 session 内容变了才会去更新 session。
+  //   resave 为 true 是每次访问都会更新 session，不管有没有修改 session 的内容，而 false 是只有 session 内容变了才会去更新 session。
 
-// saveUninitalized 设置为 true 是不管是否设置 session，都会初始化一个空的 session 对象。比如你没有登录的时候，也会初始化一个 session 对象，这个设置为 false 就好。
+  // saveUninitalized 设置为 true 是不管是否设置 session，都会初始化一个空的 session 对象。比如你没有登录的时候，也会初始化一个 session 对象，这个设置为 false 就好。
 
   // app.useLogger(new MyLogger());
 
   app.useLogger(app.get(WINSTON_LOGGER_TOKEN));
 
-  app.setGlobalPrefix('')
+  app.setGlobalPrefix('');
   await app.listen(process.env.PORT);
-  console.log(`server start success http://localhost:${process.env.PORT}`)
+  console.log(`server start success http://localhost:${process.env.PORT}`);
 }
 bootstrap();
